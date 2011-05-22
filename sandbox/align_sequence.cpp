288ecadf7ff10fd75965b60d6da549a74ddcbdc1
Francois Pomerleau <f.pomerleau@gmail.com>
2011-05-22 17:38:31 +0200

    add sandbox directory for dev test, use the flag COMPILE_SANDBOX with ccmake to activate compilation

---
+++ sandbox/align_sequence.cpp
@@ -0,0 +1,161 @@
+// kate: replace-tabs off; indent-width 4; indent-mode normal
+// vim: ts=4:sw=4:noexpandtab
+/*
+
+Copyright (c) 2010--2011,
+François Pomerleau and Stephane Magnenat, ASL, ETHZ, Switzerland
+You can contact the authors at <f dot pomerleau at gmail dot com> and
+<stephane at magnenat dot net>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL ETH-ASL BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "pointmatcher/PointMatcher.h"
+#include <cassert>
+#include <iostream>
+#include <fstream>
+#include <boost/format.hpp>
+
+using namespace std;
+
+void validateArgs(int argc, char *argv[]);
+void setupArgs(int argc, char *argv[], unsigned int& startId, unsigned int& endId, string& extension);
+
+/**
+  * Code example for ICP taking a sequence of point clouds relatively close 
+  * and computing the transformation between them.
+  */
+int main(int argc, char *argv[])
+{
+	validateArgs(argc, argv);
+
+	unsigned int startId, endId;
+	string extension = "vtk";
+	setupArgs(argc, argv, startId, endId, extension);
+	
+	
+
+	typedef MetricSpaceAligner<float> MSA;
+	
+	const string inputBaseFileName(argv[1]);
+	const string outputBaseFileName(argv[2]);
+	
+	
+	// Main algorithm definition
+	MSA::ICPSequence icp(3);
+
+	icp.setDefault();
+
+	typedef MSA::TransformationParameters TP;
+	typedef MSA::DataPoints DP;
+	
+	
+	MSA::DataPoints lastCloud, newCloud;
+	MSA::TransformFeatures transform;
+	TP tp;
+	for (unsigned frameCounter = startId; frameCounter < endId; ++frameCounter)
+	{
+		const string inputFileName((boost::format("%s.%05d.%s") % inputBaseFileName % frameCounter % extension).str());
+		const string outputFileName((boost::format("%s.%05d.vtk") % outputBaseFileName % frameCounter).str());
+
+		ifstream ifs(inputFileName.c_str());
+		if (!ifs.good())
+		{
+			cout << "Stopping at frame " << frameCounter << endl;
+			cout << "No file named " << inputFileName << endl;
+			break;
+		}
+
+		if(frameCounter == 3)
+			abort();
+
+		if(extension == "vtk")
+			newCloud = loadVTK<MSA::ScalarType>(ifs);
+		else if (extension == "csv")
+			newCloud = loadCSV<MSA::ScalarType>(ifs);
+		else
+		{
+			cerr << "Unkowned extension" << endl;
+			abort();
+		}
+		
+		// call icp
+		try 
+		{
+			tp = icp(newCloud);
+			//tp = icp.getDeltaTransform();
+			//cout << "Transformation: "<< tp << endl;
+			cout << "match ratio: " << icp.errorMinimizer->getWeightedPointUsedRatio() << endl;
+			
+			newCloud = transform.compute(newCloud, tp);
+		}
+		catch (MSA::ConvergenceError error)
+		{
+			cout << "ERROR MSA::ICP failed to converge: " << endl;
+			cout << "   " << error.what() << endl;
+			cout << "Reseting tracking" << endl;
+			icp.resetTracking(newCloud);
+		}
+		
+		cout << "outputFileName: " << outputFileName << endl;
+		saveVTK<MSA::ScalarType>(newCloud, outputFileName);
+	}
+	return 0;
+}
+
+void validateArgs(int argc, char *argv[])
+{
+	if (!(argc == 3 || argc == 5 || argc == 6))
+	{
+		cerr << "Error in command line, usage " << argv[0] << " inputBaseFileName outputBaseFileName [startId endId] [-csv]" << endl;
+		cerr << endl << "Example:" << endl;
+		cerr << argv[0] << " ../examples/data/cloud ./cloud_out" << endl << endl;
+
+		abort();
+	}
+}
+
+
+void setupArgs(int argc, char *argv[], unsigned int& startId, unsigned int& endId, string& extension)
+{
+	if(argc >= 5)
+	{
+		startId = atoi(argv[3]);
+		endId = atoi(argv[4]);
+	}
+	else
+	{
+		startId = 0;
+		endId = 1000;
+	}
+
+	if(argc == 6)
+	{
+		const string command(argv[5]);
+		if(command == "-csv")
+			extension = "csv";
+	}
+}
---
+++ sandbox/icp.cpp
@@ -0,0 +1,166 @@
+// kate: replace-tabs off; indent-width 4; indent-mode normal
+// vim: ts=4:sw=4:noexpandtab
+/*
+
+Copyright (c) 2010--2011,
+François Pomerleau and Stephane Magnenat, ASL, ETHZ, Switzerland
+You can contact the authors at <f dot pomerleau at gmail dot com> and
+<stephane at magnenat dot net>
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of the <organization> nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL ETH-ASL BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+#include "pointmatcher/PointMatcher.h"
+#include <cassert>
+#include <iostream>
+#include "boost/filesystem.hpp"
+
+using namespace std;
+
+void validateArgs(int argc, char *argv[], bool& isCSV);
+void basicUsage(char *argv[]);
+
+/**
+  * Code example for ICP taking 2 points clouds (2D or 3D) relatively close 
+  * and computing the transformation between them.
+  */
+int main(int argc, char *argv[])
+{
+	bool isCSV = true;
+	validateArgs(argc, argv, isCSV);
+	
+	typedef MetricSpaceAligner<float> MSA;
+	
+	
+	// Load point clouds
+	MSA::DataPoints ref;
+	MSA::DataPoints data;
+	if(isCSV)
+	{
+		ref = loadCSV<MSA::ScalarType>(argv[1]);
+		data = loadCSV<MSA::ScalarType>(argv[2]);
+	}
+	else
+	{
+		ref = loadVTK<MSA::ScalarType>(argv[1]);
+		data= loadVTK<MSA::ScalarType>(argv[2]);
+	}
+
+
+	// Create the default ICP algorithm
+	MSA::ICP icp;
+	// See the implementation of setDefault() to create a custom ICP algorithm
+	icp.setDefault();
+
+	// Modify the default Inspector to output vtk file
+	if(argc == 4)
+	{
+		string baseFolder(argv[3]);
+		icp.inspector = new MSA::VTKFileInspector(baseFolder + "test");
+	}
+	
+	// Compute the transformation to express data in ref
+	MSA::TransformationParameters T = icp(data, ref);
+
+	// Transform data to express it in ref
+	MSA::TransformFeatures transform;
+	MSA::DataPoints data_out = transform.compute(data, T);
+	
+	// Safe files to see the results
+	saveVTK<MSA::ScalarType>(ref, "test_ref.vtk");
+	saveVTK<MSA::ScalarType>(data, "test_data_in.vtk");
+	saveVTK<MSA::ScalarType>(data_out, "test_data_out.vtk");
+	cout << "Final transformation:" << endl << T << endl;
+
+	return 0;
+}
+
+
+void validateArgs(int argc, char *argv[], bool& isCSV )
+{
+	if (argc == 2)
+	{
+		string cmd(argv[1]);
+		if(cmd == "--help")
+		{
+			basicUsage(argv);
+			cerr << "Will create 3 vtk files for inspection: ./test_ref.vtk, ./test_data_in.vtk and ./test_data_out.vtk" << endl;
+			cerr << endl << "2D Example:" << endl;
+			cerr << "  " << argv[0] << " ../examples/data/2D_twoBoxes.csv ../examples/data/2D_oneBox.csv" << endl;
+			cerr << endl << "3D Example:" << endl;
+			cerr << "  " << argv[0] << " ../examples/data/car_cloud400.csv ../examples/data/car_cloud401.csv" << endl;
+			cerr << endl << "If you enter optional REPOSITORY name, a vtk file will be created for every iteration in that repository"	<< endl << endl;
+
+			abort();
+
+		}
+	}
+	if (!(argc == 3 || argc == 4))
+	{
+		basicUsage(argv);
+		cerr << "Use " << argv[0] << " --help for more info" << endl << endl; 
+		
+		abort();
+	}
+	
+	// Validate extension
+	const boost::filesystem::path pathRef(argv[1]);
+	const boost::filesystem::path pathData(argv[2]);
+
+	string refExt = boost::filesystem::extension(pathRef);
+	string dataExt = boost::filesystem::extension(pathData);
+
+	if (!(refExt == ".vtk" || refExt == ".csv"))
+	{
+		cout << refExt << ", " << dataExt << endl;
+		cerr << "Reference file extension must be .vtk or .csv" << endl;
+		abort();
+	}
+	
+	if (!(dataExt == ".vtk" || dataExt == ".csv"))
+	{
+		cerr << "Reading file extension must be .vtk or .csv" << endl;
+		abort();
+	}
+
+	if (dataExt != refExt)
+	{
+		cerr << "File extension between reference and reading should be the same" << endl;
+		abort();
+	}
+
+	if (dataExt == ".csv")
+		isCSV = true;
+	else
+		isCSV = false;
+}
+
+void basicUsage(char *argv[])
+{
+	cerr << endl;
+	cerr << "Error in command line, usage " << argv[0] << " reference.csv reading.csv [REPOSITORY]" << endl;
+	cerr << endl;
+}
