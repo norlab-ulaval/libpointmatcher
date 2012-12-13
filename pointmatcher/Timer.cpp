8adf01229efcf70a6802c9fc3c429cac000a1d82
Stéphane Magnenat <stephane@magnenat.net>
2012-12-12 16:21:44 -0800

    Applied OS X patch from Vincent Regis

--- pointmatcher/Timer.cpp
+++ pointmatcher/Timer.cpp
@@ -35,6 +35,11 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include "Timer.h"
 
+#ifdef __MACH__
+#include <mach/clock.h>
+#include <mach/mach.h>
+#endif
+
 #ifdef _POSIX_TIMERS
 namespace PointMatcherSupport
 {
@@ -55,9 +60,17 @@ namespace PointMatcherSupport
 	
 	timer::Time timer::curTime() const
 	{
+		#ifdef __MACH__ // OS X does not have clock_gettime, use clock_get_time
+		clock_serv_t host_clock;
+		mach_timespec_t now;
+		host_get_clock_service(mach_host_self(), SYSTEM_CLOCK, &host_clock);
+		clock_get_time(host_clock, &now);
+		return Time(now.tv_sec) * Time(1000000000) + Time(now.tv_nsec);
+		#else // __MACH__
 		struct timespec ts;
 		clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &ts);
 		return Time(ts.tv_sec) * Time(1000000000) + Time(ts.tv_nsec);
+		#endif // __MACH__
 	}
 } // namespace PointMatcherSupport
 #endif // _POSIX_TIMERS
