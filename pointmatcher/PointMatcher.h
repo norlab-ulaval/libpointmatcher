f15403c42eafacf35e7f762560fed13af87e15af
Stéphane Magnenat <stephane@magnenat.net>
2013-09-04 13:55:58 +0200

    Updated version number.

--- pointmatcher/PointMatcher.h
+++ pointmatcher/PointMatcher.h
@@ -68,9 +68,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 //! version of the Pointmatcher library as string
-#define POINTMATCHER_VERSION "1.0.2"
+#define POINTMATCHER_VERSION "1.1.0"
 //! version of the Pointmatcher library as an int
-#define POINTMATCHER_VERSION_INT 10002
+#define POINTMATCHER_VERSION_INT 10100
 
 //! Functions and classes that are not dependant on scalar type are defined in this namespace
 namespace PointMatcherSupport
