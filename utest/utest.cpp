bce501cdb9896c448497d936bc40f7db60cb0db4
Samuel Charreyron <samlcharreyron@gmail.com>
2014-04-17 18:15:30 +0200

    new unit tests for loading/saving different file formats

--- utest/utest.cpp
+++ utest/utest.cpp
@@ -366,78 +366,95 @@ TEST(IOTest, loadYaml)
 	EXPECT_THROW(icp.loadFromYaml(ifs3), PointMatcherSupport::InvalidModuleType);
 }
 
-
-TEST(IOTest, loadSaveVTK)
+class IOLoadSaveTest : public testing::Test
 {
-	const int nbPts = 10;
-	DP::Labels featureLabels;
-	featureLabels.push_back(DP::Label("x", 1));
-	
-	// Generate fake point cloud
-	//DP ptCloud(PM::Matrix::Random(1, nbPts), featureLabels);
-	DP ptCloud;
-	ptCloud.addFeature("x", PM::Matrix::Random(1, nbPts));
-	ptCloud.addFeature("y", PM::Matrix::Random(1, nbPts));
-	ptCloud.addFeature("z", PM::Matrix::Random(1, nbPts));
-	ptCloud.addFeature("pad", PM::Matrix::Ones(1, nbPts));
 
-	// Set up descriptors
-	ptCloud.addDescriptor("normal", PM::Matrix::Random(3, nbPts));
-	ptCloud.addDescriptor("eigVectors", PM::Matrix::Random(9, nbPts));
-	ptCloud.addDescriptor("color", PM::Matrix::Random(4, nbPts));
+public:
+	virtual void SetUp()
+	{
+		nbPts = 10;
+		addRandomFeature("x", 1);
+		addRandomFeature("y", 1);
+		addRandomFeature("z", 1);
+		ptCloud.addFeature("pad", PM::Matrix::Ones(1, nbPts));
+
+		addRandomDescriptor("normals",3);
+		addRandomDescriptor("eigValues",3);
+		addRandomDescriptor("eigVectors",9);
+		addRandomDescriptor("color",4);
+	}
+
+	void addRandomFeature(const string& featureName, const int rows)
+	{
+		ptCloud.addFeature(featureName,PM::Matrix::Random(rows, nbPts));
+	}
+
+	void addRandomDescriptor(const string& descriptorName, const int rows)
+	{
+		ptCloud.addDescriptor(descriptorName, PM::Matrix::Random(rows, nbPts));
+	}
+
+	virtual void loadSaveTest(const string& testFileName, const int nbPts = 10)
+	{
+		this->testFileName = testFileName;
+		ptCloud.save(testFileName);
+
+		ptCloudFromFile = DP::load(testFileName);
+
+		EXPECT_TRUE(ptCloudFromFile.features.cols() == ptCloud.features.cols());
+		EXPECT_TRUE(ptCloudFromFile.descriptorExists("normals",3));
+		EXPECT_TRUE(ptCloudFromFile.getDescriptorViewByName("normals").isApprox(ptCloud.getDescriptorViewByName("normals")));
+		EXPECT_TRUE(ptCloudFromFile.descriptorExists("eigValues",3));
+		EXPECT_TRUE(ptCloudFromFile.getDescriptorViewByName("eigValues").isApprox(ptCloud.getDescriptorViewByName("eigValues")));
+		EXPECT_TRUE(ptCloudFromFile.descriptorExists("eigVectors",9));
+		EXPECT_TRUE(ptCloudFromFile.getDescriptorViewByName("eigVectors").isApprox(ptCloud.getDescriptorViewByName("eigVectors")));
+		EXPECT_TRUE(ptCloudFromFile.descriptorExists("color",4));
+		EXPECT_TRUE(ptCloudFromFile.getDescriptorViewByName("color").isApprox(ptCloud.getDescriptorViewByName("color")));
+
+		EXPECT_TRUE(ptCloudFromFile.features.isApprox(ptCloud.features));
+
+	}
+
+	virtual void TearDown()
+	{
+		EXPECT_TRUE(boost::filesystem::remove(boost::filesystem::path(testFileName)));
+	}
+
+
+protected:
+	int nbPts;
+	DP::Labels featureLabels;
+	DP ptCloud;
+	DP ptCloudFromFile;
+	string testFileName;
+
+};
+
+TEST_F(IOLoadSaveTest, VTK)
+{
 	ptCloud.addDescriptor("genericScalar", PM::Matrix::Random(1, nbPts));
 	ptCloud.addDescriptor("genericVector", PM::Matrix::Random(3, nbPts));
 
-	string testPathName = "unit_test.vtk";
-	ptCloud.save(testPathName);
+	loadSaveTest("unit_test.vtk");
 
-	DP ptCloudFromFile(DP::load(testPathName));
+	EXPECT_TRUE(ptCloudFromFile.descriptorExists("genericScalar",1));
+	EXPECT_TRUE(ptCloudFromFile.descriptorExists("genericVector",3));
 
-	EXPECT_TRUE(ptCloudFromFile.features.cols() == ptCloud.features.cols());
-	EXPECT_TRUE(ptCloudFromFile.descriptorExists("normal"));
-	EXPECT_TRUE(ptCloudFromFile.descriptorExists("eigVectors"));
-	EXPECT_TRUE(ptCloudFromFile.descriptorExists("color"));
-	EXPECT_TRUE(ptCloudFromFile.descriptorExists("genericScalar"));
-	EXPECT_TRUE(ptCloudFromFile.descriptorExists("genericVector"));
-
-	//TODO: check with a known file if the values are correct
-
-	// Remove file from disk
-	EXPECT_TRUE(boost::filesystem::remove(boost::filesystem::path(testPathName)));
 }
 
-TEST(IOTest, loadSavePLY)
+TEST_F(IOLoadSaveTest, PLY)
 {
-	const int nbPts = 10;
-	DP::Labels featureLabels;
-	featureLabels.push_back(DP::Label("x", 1));
+	loadSaveTest("unit_test.ply");
+}
 
-	// Generate fake point cloud
-	//DP ptCloud(PM::Matrix::Random(1, nbPts), featureLabels);
-	DP ptCloud;
-	ptCloud.addFeature("x", PM::Matrix::Random(1, nbPts));
-	ptCloud.addFeature("y", PM::Matrix::Random(1, nbPts));
-	ptCloud.addFeature("z", PM::Matrix::Random(1, nbPts));
-	ptCloud.addFeature("pad", PM::Matrix::Ones(1, nbPts));
+TEST_F(IOLoadSaveTest, PCD)
+{
+	loadSaveTest("unit_test.pcd");
+}
 
-	// Set up descriptors
-	ptCloud.addDescriptor("normals", PM::Matrix::Random(3, nbPts));
-	//ptCloud.addDescriptor("eigVectors", PM::Matrix::Random(3, nbPts));
-	ptCloud.addDescriptor("color", PM::Matrix::Random(4, nbPts));
-
-	string testPathName = "unit_test.ply";
-	ptCloud.save(testPathName);
-
-	DP ptCloudFromFile(DP::load(testPathName));
-
-	EXPECT_TRUE(ptCloudFromFile.features.cols() == ptCloud.features.cols());
-	EXPECT_TRUE(ptCloudFromFile.descriptorExists("normals"));
-	//EXPECT_TRUE(ptCloudFromFile.descriptorExists("eigVectors"));
-	EXPECT_TRUE(ptCloudFromFile.descriptorExists("color"));
-
-	// Remove file from disk
-	EXPECT_TRUE(boost::filesystem::remove(boost::filesystem::path(testPathName)));
-
+TEST_F(IOLoadSaveTest, CSV)
+{
+	loadSaveTest("unit_test.csv");
 }
 
 
