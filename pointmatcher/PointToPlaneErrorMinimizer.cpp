9fa974791d6363d4145c21c8a574a8900d2c58df
David Landry <davidlandry93@gmail.com>
2017-05-11 11:53:23 +0200

    Now have a shared constructor

--- pointmatcher/PointToPlaneErrorMinimizer.cpp
+++ pointmatcher/PointToPlaneErrorMinimizer.cpp
@@ -20,7 +20,14 @@ typedef Parametrizable::ParametersDoc ParametersDoc;
 
 template<typename T>
 PointToPlaneErrorMinimizer<T>::PointToPlaneErrorMinimizer(const Parameters& params):
-	ErrorMinimizer("PointToPlaneErrorMinimizer", PointToPlaneErrorMinimizer::availableParameters(), params),
+	ErrorMinimizer(name(), availableParameters(), params),
+	force2D(Parametrizable::get<T>("force2D"))
+{
+}
+
+template<typename T>
+PointToPlaneErrorMinimizer<T>::PointToPlaneErrorMinimizer(const ParametersDoc paramsDoc, const Parameters& params):
+	ErrorMinimizer(name(), paramsDoc, params),
 	force2D(Parametrizable::get<T>("force2D"))
 {
 }
@@ -81,121 +88,123 @@ void solvePossiblyUnderdeterminedLinearSystem(const MatrixA& A, const Vector & b
 	}
 }
 
-
-
 template<typename T>
 typename PointMatcher<T>::TransformationParameters PointToPlaneErrorMinimizer<T>::compute(const ErrorElements& mPts_const)
 {
-	// Copy error element to use as storage later
-	// TODO: check that, might worth it to only copy useful parts
-	ErrorElements mPts = mPts_const;
+		ErrorElements mPts = mPts_const;
+		return compute_in_place(mPts);
+}
 
-	const int dim = mPts.reading.features.rows();
-	const int nbPts = mPts.reading.features.cols();
 
-	// Adjust if the user forces 2D minimization on XY-plane
-	int forcedDim = dim - 1;
-	if(force2D && dim == 4)
-	{
-		mPts.reading.features.conservativeResize(3, Eigen::NoChange);
-		mPts.reading.features.row(2) = Matrix::Ones(1, nbPts);
-		mPts.reference.features.conservativeResize(3, Eigen::NoChange);
-		mPts.reference.features.row(2) = Matrix::Ones(1, nbPts);
-		forcedDim = dim - 2;
-	}
+template<typename T>
+typename PointMatcher<T>::TransformationParameters PointToPlaneErrorMinimizer<T>::compute_in_place(ErrorElements& mPts)
+{
+		const int dim = mPts.reading.features.rows();
+		const int nbPts = mPts.reading.features.cols();
 
-	// Fetch normal vectors of the reference point cloud (with adjustment if needed)
-	const BOOST_AUTO(normalRef, mPts.reference.getDescriptorViewByName("normals").topRows(forcedDim));
-
-	// Note: Normal vector must be precalculated to use this error. Use appropriate input filter.
-	assert(normalRef.rows() > 0);
-
-	// Compute cross product of cross = cross(reading X normalRef)
-	const Matrix cross = this->crossProduct(mPts.reading.features, normalRef);
-
-	// wF = [weights*cross, weights*normals]
-	// F  = [cross, normals]
-	Matrix wF(normalRef.rows()+ cross.rows(), normalRef.cols());
-	Matrix F(normalRef.rows()+ cross.rows(), normalRef.cols());
-	
-	for(int i=0; i < cross.rows(); i++)
-	{
-		wF.row(i) = mPts.weights.array() * cross.row(i).array();
-		F.row(i) = cross.row(i);
-	}
-	for(int i=0; i < normalRef.rows(); i++)
-	{
-    	        wF.row(i + cross.rows()) = mPts.weights.array() * normalRef.row(i).array();
-		F.row(i + cross.rows()) = normalRef.row(i);
-	}
-
-	// Unadjust covariance A = wF * F'
-	const Matrix A = wF * F.transpose();
-
-	const Matrix deltas = mPts.reading.features - mPts.reference.features;
-
-	// dot product of dot = dot(deltas, normals)
-	Matrix dotProd = Matrix::Zero(1, normalRef.cols());
-
-	for(int i=0; i<normalRef.rows(); i++)
-	{
-		dotProd += (deltas.row(i).array() * normalRef.row(i).array()).matrix();
-	}
-
-	// b = -(wF' * dot)
-	const Vector b = -(wF * dotProd.transpose());
-
-	Vector x(A.rows());
-
-	solvePossiblyUnderdeterminedLinearSystem<T>(A, b, x);
-
-	// Transform parameters to matrix
-	Matrix mOut;
-	if(dim == 4 && !force2D)
-	{
-		Eigen::Transform<T, 3, Eigen::Affine> transform;
-		// PLEASE DONT USE EULAR ANGLES!!!!
-		// Rotation in Eular angles follow roll-pitch-yaw (1-2-3) rule
-		/*transform = Eigen::AngleAxis<T>(x(0), Eigen::Matrix<T,1,3>::UnitX())
-				* Eigen::AngleAxis<T>(x(1), Eigen::Matrix<T,1,3>::UnitY())
-				* Eigen::AngleAxis<T>(x(2), Eigen::Matrix<T,1,3>::UnitZ());*/
-
-		transform = Eigen::AngleAxis<T>(x.head(3).norm(),x.head(3).normalized());
-
-		// Reverse roll-pitch-yaw conversion, very useful piece of knowledge, keep it with you all time!
-		/*const T pitch = -asin(transform(2,0));
-		const T roll = atan2(transform(2,1), transform(2,2));
-		const T yaw = atan2(transform(1,0) / cos(pitch), transform(0,0) / cos(pitch));
-		std::cerr << "d angles" << x(0) - roll << ", " << x(1) - pitch << "," << x(2) - yaw << std::endl;*/
-		transform.translation() = x.segment(3, 3);
-		mOut = transform.matrix();
-
-		if (mOut != mOut) 
+		// Adjust if the user forces 2D minimization on XY-plane
+		int forcedDim = dim - 1;
+		if(force2D && dim == 4)
 		{
-			// Degenerate situation. This can happen when the source and reading clouds
-			// are identical, and then b and x above are 0, and the rotation matrix cannot
-			// be determined, it comes out full of NaNs. The correct rotation is the identity.
-			mOut.block(0, 0, dim-1, dim-1) = Matrix::Identity(dim-1, dim-1);
-		}	
-	}
-	else
-	{
-		Eigen::Transform<T, 2, Eigen::Affine> transform;
-		transform = Eigen::Rotation2D<T> (x(0));
-		transform.translation() = x.segment(1, 2);
+				mPts.reading.features.conservativeResize(3, Eigen::NoChange);
+				mPts.reading.features.row(2) = Matrix::Ones(1, nbPts);
+				mPts.reference.features.conservativeResize(3, Eigen::NoChange);
+				mPts.reference.features.row(2) = Matrix::Ones(1, nbPts);
+				forcedDim = dim - 2;
+		}
 
-		if(force2D)
+		// Fetch normal vectors of the reference point cloud (with adjustment if needed)
+		const BOOST_AUTO(normalRef, mPts.reference.getDescriptorViewByName("normals").topRows(forcedDim));
+
+		// Note: Normal vector must be precalculated to use this error. Use appropriate input filter.
+		assert(normalRef.rows() > 0);
+
+		// Compute cross product of cross = cross(reading X normalRef)
+		const Matrix cross = this->crossProduct(mPts.reading.features, normalRef);
+
+		// wF = [weights*cross, weights*normals]
+		// F  = [cross, normals]
+		Matrix wF(normalRef.rows()+ cross.rows(), normalRef.cols());
+		Matrix F(normalRef.rows()+ cross.rows(), normalRef.cols());
+
+		for(int i=0; i < cross.rows(); i++)
 		{
-			mOut = Matrix::Identity(dim, dim);
-			mOut.topLeftCorner(2, 2) = transform.matrix().topLeftCorner(2, 2);
-			mOut.topRightCorner(2, 1) = transform.matrix().topRightCorner(2, 1);
+				wF.row(i) = mPts.weights.array() * cross.row(i).array();
+				F.row(i) = cross.row(i);
+		}
+		for(int i=0; i < normalRef.rows(); i++)
+		{
+				wF.row(i + cross.rows()) = mPts.weights.array() * normalRef.row(i).array();
+				F.row(i + cross.rows()) = normalRef.row(i);
+		}
+
+		// Unadjust covariance A = wF * F'
+		const Matrix A = wF * F.transpose();
+
+		const Matrix deltas = mPts.reading.features - mPts.reference.features;
+
+		// dot product of dot = dot(deltas, normals)
+		Matrix dotProd = Matrix::Zero(1, normalRef.cols());
+
+		for(int i=0; i<normalRef.rows(); i++)
+		{
+				dotProd += (deltas.row(i).array() * normalRef.row(i).array()).matrix();
+		}
+
+		// b = -(wF' * dot)
+		const Vector b = -(wF * dotProd.transpose());
+
+		Vector x(A.rows());
+
+		solvePossiblyUnderdeterminedLinearSystem<T>(A, b, x);
+
+		// Transform parameters to matrix
+		Matrix mOut;
+		if(dim == 4 && !force2D)
+		{
+				Eigen::Transform<T, 3, Eigen::Affine> transform;
+				// PLEASE DONT USE EULAR ANGLES!!!!
+				// Rotation in Eular angles follow roll-pitch-yaw (1-2-3) rule
+				/*transform = Eigen::AngleAxis<T>(x(0), Eigen::Matrix<T,1,3>::UnitX())
+				 * Eigen::AngleAxis<T>(x(1), Eigen::Matrix<T,1,3>::UnitY())
+				 * Eigen::AngleAxis<T>(x(2), Eigen::Matrix<T,1,3>::UnitZ());*/
+
+				transform = Eigen::AngleAxis<T>(x.head(3).norm(),x.head(3).normalized());
+
+				// Reverse roll-pitch-yaw conversion, very useful piece of knowledge, keep it with you all time!
+				/*const T pitch = -asin(transform(2,0));
+					const T roll = atan2(transform(2,1), transform(2,2));
+					const T yaw = atan2(transform(1,0) / cos(pitch), transform(0,0) / cos(pitch));
+					std::cerr << "d angles" << x(0) - roll << ", " << x(1) - pitch << "," << x(2) - yaw << std::endl;*/
+				transform.translation() = x.segment(3, 3);
+				mOut = transform.matrix();
+
+				if (mOut != mOut)
+				{
+						// Degenerate situation. This can happen when the source and reading clouds
+						// are identical, and then b and x above are 0, and the rotation matrix cannot
+						// be determined, it comes out full of NaNs. The correct rotation is the identity.
+						mOut.block(0, 0, dim-1, dim-1) = Matrix::Identity(dim-1, dim-1);
+				}
 		}
 		else
 		{
-			mOut = transform.matrix();
+				Eigen::Transform<T, 2, Eigen::Affine> transform;
+				transform = Eigen::Rotation2D<T> (x(0));
+				transform.translation() = x.segment(1, 2);
+
+				if(force2D)
+				{
+						mOut = Matrix::Identity(dim, dim);
+						mOut.topLeftCorner(2, 2) = transform.matrix().topLeftCorner(2, 2);
+						mOut.topRightCorner(2, 1) = transform.matrix().topRightCorner(2, 1);
+				}
+				else
+				{
+						mOut = transform.matrix();
+				}
 		}
-	}
-	return mOut; 
+		return mOut;
 }
 
 
--- pointmatcher/PointToPlaneWithCovErrorMinimizer.h
+++ pointmatcher/PointToPlaneWithCovErrorMinimizer.h
@@ -1,48 +1,54 @@
+#ifndef POINT_TO_PLANE_WITH_COV_ERROR_MINIMIZER_H
+#define POINT_TO_PLANE_WITH_COV_ERROR_MINIMIZER_H
 
 #include "PointMatcher.h"
-
+#include "PointToPlaneErrorMinimizer.h"
 
 template<typename T>
-struct PointToPlaneWithCovErrorMinimizer: public PointMatcher<T>::ErrorMinimizer
+struct PointToPlaneWithCovErrorMinimizer: public PointToPlaneErrorMinimizer<T>
 {
-		typedef PointMatcherSupport::Parametrizable Parametrizable;
-		typedef PointMatcherSupport::Parametrizable P;
-		typedef Parametrizable::Parameters Parameters;
-		typedef Parametrizable::ParameterDoc ParameterDoc;
-		typedef Parametrizable::ParametersDoc ParametersDoc;
+    typedef PointMatcherSupport::Parametrizable Parametrizable;
+    typedef PointMatcherSupport::Parametrizable P;
+    typedef Parametrizable::Parameters Parameters;
+    typedef Parametrizable::ParameterDoc ParameterDoc;
+    typedef Parametrizable::ParametersDoc ParametersDoc;
 
-		typedef typename PointMatcher<T>::DataPoints DataPoints;
-		typedef typename PointMatcher<T>::Matches Matches;
-		typedef typename PointMatcher<T>::OutlierWeights OutlierWeights;
-		typedef typename PointMatcher<T>::ErrorMinimizer ErrorMinimizer;
-		typedef typename PointMatcher<T>::ErrorMinimizer::ErrorElements ErrorElements;
-		typedef typename PointMatcher<T>::TransformationParameters TransformationParameters;
-		typedef typename PointMatcher<T>::Vector Vector;
-		typedef typename PointMatcher<T>::Matrix Matrix;
+    typedef typename PointMatcher<T>::DataPoints DataPoints;
+    typedef typename PointMatcher<T>::Matches Matches;
+    typedef typename PointMatcher<T>::OutlierWeights OutlierWeights;
+    typedef typename PointMatcher<T>::ErrorMinimizer ErrorMinimizer;
+    typedef typename PointMatcher<T>::ErrorMinimizer::ErrorElements ErrorElements;
+    typedef typename PointMatcher<T>::TransformationParameters TransformationParameters;
+    typedef typename PointMatcher<T>::Vector Vector;
+    typedef typename PointMatcher<T>::Matrix Matrix;
 
-		inline static const std::string description()
-		{
-				return "Point-to-plane error (or point-to-line in 2D). Based on \\cite{Chen1991Point2Plane}. Covariance estimation based on \\cite{Censi2007ICPCovariance}.";
-		}
+    virtual inline const std::string name()
+    {
+        return "PointToPlaneWithCovErrorMinimizer";
+    }
 
-		inline static const ParametersDoc availableParameters()
-		{
-				return boost::assign::list_of<ParameterDoc>
-						( "force2D", "If set to true(1), the minimization will be force to give a solution in 2D (i.e., on the XY-plane) even with 3D inputs.", "0", "0", "1", &P::Comp<bool>)
-						( "sensorStdDev", "sensor standard deviation", "0.01", "0.", "inf", &P::Comp<T>)
-						;
-		}
+    inline static const std::string description()
+    {
+        return "Point-to-plane error (or point-to-line in 2D). Based on \\cite{Chen1991Point2Plane}. Covariance estimation based on \\cite{Censi2007ICPCovariance}.";
+    }
 
-		const bool force2D;
-		const T sensorStdDev;
-		Matrix covMatrix;
+    static inline const ParametersDoc availableParameters()
+    {
+        return boost::assign::list_of<ParameterDoc>
+            ( "force2D", "If set to true(1), the minimization will be force to give a solution in 2D (i.e., on the XY-plane) even with 3D inputs.", "0", "0", "1", &P::Comp<bool>)
+            ( "sensorStdDev", "sensor standard deviation", "0.01", "0.", "inf", &P::Comp<T>)
+            ;
+    }
 
-		PointToPlaneWithCovErrorMinimizer(const Parameters& params = Parameters());
-		virtual TransformationParameters compute(const ErrorElements& mPts);
-		virtual T getResidualError(const DataPoints& filteredReading, const DataPoints& filteredReference, const OutlierWeights& outlierWeights, const Matches& matches) const;
-		virtual T getOverlap() const;
-		virtual Matrix getCovariance() const;
-		Matrix estimateCovariance(const ErrorElements& mPts, const TransformationParameters& transformation);
+    const T sensorStdDev;
+    Matrix covMatrix;
+
+    PointToPlaneWithCovErrorMinimizer(const Parameters& params = Parameters());
+    virtual TransformationParameters compute(const ErrorElements& mPts);
+    virtual T getResidualError(const DataPoints& filteredReading, const DataPoints& filteredReference, const OutlierWeights& outlierWeights, const Matches& matches) const;
+    virtual T getOverlap() const;
+    virtual Matrix getCovariance() const;
+    Matrix estimateCovariance(const ErrorElements& mPts, const TransformationParameters& transformation);
 };
 
-
+#endif
