25b787db8cccded80a1c7de0a24d860372bbfd30
Vladimir Kubelka <kubelvla@gmail.com>
2020-04-22 17:37:13 -0400

    Final cleanup.

--- pointmatcher/DataPointsFilters/Geometry.cpp
+++ pointmatcher/DataPointsFilters/Geometry.cpp
@@ -2,8 +2,6 @@
 // vim: ts=4:sw=4:noexpandtab
 /*
 
-//TODO!!!
-
 Copyright (c) 2010--2018,
 François Pomerleau and Stephane Magnenat, ASL, ETHZ, Switzerland
 You can contact the authors at <f dot pomerleau at gmail dot com> and
