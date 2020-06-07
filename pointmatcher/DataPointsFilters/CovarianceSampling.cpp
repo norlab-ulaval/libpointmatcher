e7bacd51a5397ca6f974f8b28dbb2332b62bd701
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2020-06-07 17:41:56 +0200

    Another out-of-bounds fixed

--- pointmatcher/DataPointsFilters/CovarianceSampling.cpp
+++ pointmatcher/DataPointsFilters/CovarianceSampling.cpp
@@ -107,7 +107,7 @@ void CovarianceSamplingDataPointsFilter<T>::inPlaceFilter(DataPoints& cloud)
 		for (std::size_t f = 0; f < 3; ++f)
 			center(f) += cloud.features(f,candidates[i]);
 	
-	for(std::size_t i = 0; i <= 3; ++i) center(i) /= T(nbCandidates);
+	for(std::size_t i = 0; i < 3; ++i) center(i) /= T(nbCandidates);
 	
 	//Compute torque normalization
 	T Lnorm = 1.0;
