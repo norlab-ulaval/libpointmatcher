3ccaf53be8c66c0186f3befadf43e9fcc3677a6f
Alexandre Guénette <alexandre.guenette.1@ulaval.ca>
2021-07-27 16:59:02 -0400

    Add the covariance and weightSum fields to SimilarityTransformation

--- pointmatcher/TransformationsImpl.cpp
+++ pointmatcher/TransformationsImpl.cpp
@@ -251,6 +251,24 @@ void TransformationsImpl<T>::SimilarityTransformation::inPlaceCompute(
 				cloud.descriptors.block(vectorStartingRow, 0, vectorSpan, descCols).applyOnTheLeft(R);
 			}
 		}
+		else if(descName == "covariance" || descName == "weightSum")
+		{
+			int vectorSpan = std::sqrt(descSpan);
+
+			for(size_t j = 0; j < cloud.getNbPoints(); ++j)
+			{
+				typename PointMatcher<T>::Matrix descriptorMatrix = PointMatcher<T>::Matrix::Zero(vectorSpan, vectorSpan);
+				for(size_t k = 0; k < vectorSpan; ++k)
+				{
+					descriptorMatrix.col(k) = cloud.descriptors.block(descStartingRow + k * vectorSpan, j, vectorSpan, 1);
+				}
+				descriptorMatrix = R * descriptorMatrix * R.transpose();
+				for(size_t k = 0; k < vectorSpan; ++k)
+				{
+					cloud.descriptors.block(descStartingRow + k * vectorSpan, j, vectorSpan, 1) = descriptorMatrix.col(k);
+				}
+			}
+		}
 
 		descStartingRow += descSpan;
 	}
