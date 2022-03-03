0cdd7addbf2436354dfdefe1a577ebecc2d80cb5
maximecharriere <51918753+maximecharriere@users.noreply.github.com>
2022-03-03 15:47:19 +0100

    Move PointMatcher.h include in PointMatcherPrivate.h

--- pointmatcher/Histogram.cpp
+++ pointmatcher/Histogram.cpp
@@ -32,7 +32,6 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 */
-#include "PointMatcher.h"
 #include "PointMatcherPrivate.h"
 
 #include "Histogram.h"
