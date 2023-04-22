7a84f03b9fa51267eb4e50c0bda85158b247cfc1
KernelA <17554646+KernelA@users.noreply.github.com>
2023-04-22 22:51:11 +0300

    Dynamically parse vresion from file

--- python/setup.py
+++ python/setup.py
@@ -7,8 +7,23 @@ class BinaryDistribution(Distribution):
     def has_ext_modules(self):
         return True
 
+path_to_file = "../pointmatcher/PointMatcher.h"
+
+version = None
+
+with open(path_to_file, "r", encoding="utf-8") as f:
+    for line in map(str.strip, f):
+        if line.startswith("#define POINTMATCHER_VERSION"):
+            version = line.split()[2]
+            break
+
+if version is None:
+    raise RuntimeError(f"Cannot find a version string in the file: '{path_to_file}'")
+
+
 setup(packages=find_packages(include=["pypointmatcher*"]),
     install_requires=["numpy>=1.20"],
     package_data={"": ["*.so"]},
+    version=version,
     distclass=BinaryDistribution
  )
