bf0128686a39c7638a78f16dc5d7e603ead64ac4
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-06-21 15:33:42 -0400

    NMO-201, NMO-203 | Implemented general case CD components (Ubuntu x86) › install script + container
    - Implemented container+install script with libpointmatcher dependencies (Ubuntu x86 case);
    - Implemented libpointmatcher container+install script (Ubuntu x86 case);

---
+++ build_system/ubuntu/install_dependencies_ubuntu.bash
@@ -0,0 +1,110 @@
+#!/bin/bash -i
+
+# Note on unit test:
+#    $ docker pull --platform linux/arm64 ubuntu:20.04
+#    $ docker build --platform linux/arm64 -f Dockerfile.dependencies -t test-libpointmatcher-dependencies:ubuntu.20.04 .
+#    $ docker run -a --name iAmTestLibpointmatcherDependenciesContainer -t -i test-libpointmatcher-dependencies:ubuntu.20.04
+
+set -e
+
+# Load environment variable from file
+set -o allexport; source ../.env; set +o allexport
+
+
+# skip GUI dialog by setting everything to default
+export DEBIAN_FRONTEND=noninteractive
+
+# .... Create required dir structure ...................................................................................
+mkdir -p "${INSTALLED_LIBRARIES_PATH}"
+cd "${INSTALLED_LIBRARIES_PATH}"
+
+# ==== Install tools ===================================================================================================
+
+# ... install development utilities ....................................................................................
+sudo apt-get update &&
+  sudo apt-get install --assume-yes \
+    lsb-release \
+    curl \
+    wget \
+    git \
+    g++ \
+    gcc \
+    make \
+    cmake \
+    cmake-gui \
+    build-essential \
+    ca-certificates &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+sudo apt-get update &&
+  sudo apt-get install --assume-yes \
+    python3-dev \
+    python3-pip &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+python3 -m pip install --upgrade pip
+
+## ToDo: assessment >> check if next bloc ↓↓ is needed
+#sudo apt-get update \
+#    && sudo apt-get install --assume-yes \
+#        python3-opengl \
+#        python3-numpy \
+#        python-is-python3 \
+#        python3-vcstool \
+#    && sudo rm -rf /var/lib/apt/lists/*;
+
+# ==== Install Libpointmatcher dependencies ============================================================================
+# . . Install boost. . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .
+# https://www.boost.org/doc/libs/1_79_0/more/getting_started/unix-variants.html
+sudo apt-get update &&
+  sudo apt-get install --assume-yes \
+    libboost-all-dev &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+# . . Install eigen . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+# https://eigen.tuxfamily.org/index.php
+sudo apt-get update &&
+  sudo apt-get install --assume-yes \
+    libeigen3-dev &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+# . . Install libnabo . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . ..
+# https://github.com/ethz-asl/libnabo
+# ToDo: assessment >> ANN and FLANN should be required only for `make test` benchmarks
+
+## Note:ANN was not mentionned in doc --> probably because it's only used in benchmark test
+## ANN is a library written in C++, which supports data structures and algorithms for both exact and approximate nearest neighbor searching in arbitrarily high dimensions.
+## https://www.cs.umd.edu/~mount/ANN/
+#cd "${INSTALLED_LIBRARIES_PATH}"
+#wget https://www.cs.umd.edu/~mount/ANN/Files/1.1.2/ann_1.1.2.tar.gz
+#tar xzf ann_1.1.2.tar.gz
+#cd ann_1.1.2/
+#make linux-g++
+#sudo cp lib/libANN.a /usr/local/lib/
+#sudo cp include/ANN/ANN.h /usr/local/include/
+## shellcheck disable=SC2103
+#cd ..
+#
+#
+## Note:FLANN was not mentionned in doc --> probably because it's only used in benchmark test
+## Fast Library for Approximate Nearest Neighbors - development
+## FLANN is a library for performing fast approximate nearest neighbor searches in high dimensional spaces.
+## https://github.com/flann-lib/flann
+#sudo apt-get update \
+#    && sudo apt-get install --assume-yes \
+#        libflann-dev \
+#    && sudo rm -rf /var/lib/apt/lists/*
+
+cd "${INSTALLED_LIBRARIES_PATH}"
+git clone https://github.com/ethz-asl/libnabo.git &&
+  cd libnabo &&
+  mkdir build && cd build &&
+  cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo .. &&
+  make -j $(nproc) &&
+  make test &&
+  sudo make install
+
+#    && git checkout 1.0.7 \
+
+# ToDo:on task end >> next bloc ↓↓
+#pwd && tree -L 3
---
+++ build_system/ubuntu/install_libpointmatcher_ubuntu.bash
@@ -0,0 +1,120 @@
+#!/bin/bash -i
+#
+# Note:
+#   - this script required package: g++, make, cmake, build-essential, git and all libpointmatcher dependencies
+#   - execute `install_dependencies_ubuntu.bash` first
+
+# Note on unit test:
+#    $ docker pull --platform linux/arm64 test-libpointmatcher-dependencies:ubuntu.20.04
+#    $ docker build --platform linux/arm64 -f Dockerfile.libpointmatcher -t test-libpointmatcher:ubuntu.20.04 .
+#    $ docker run -a --name iAmTestLibpointmatcherContainer -t -i test-libpointmatcher:ubuntu.20.04
+
+set -e
+
+# Load environment variable from file
+set -o allexport; source ../.env; set +o allexport
+
+
+# skip GUI dialog by setting everything to default
+export DEBIAN_FRONTEND=noninteractive
+
+# .... Create required dir structure .............................................................................
+
+mkdir -p "${INSTALLED_LIBRARIES_PATH}"
+cd "${INSTALLED_LIBRARIES_PATH}"
+
+
+# ....helper function..............................................................................................
+SP="    "
+
+function print_help_in_terminal() {
+
+  echo -e "\$ ${0} [<optional argument>]
+
+${SP}
+${SP}\033[1m<optional argument>:\033[0m
+${SP}  -h, --help         Get help
+${SP}  --compile-test     Compile the libpointmatcher unit-test in ${INSTALLED_LIBRARIES_PATH}/libpointmatcher/build
+${SP}  --generate-doc     Generate the libpointmatcher doc in /usr/local/share/doc/libpointmatcher/api/html/index.html
+
+"
+}
+
+# ....Script command line flags....................................................................................
+
+## todo: on task end >> comment next dev bloc ↓↓
+#echo "${0}: all arg >>" \
+#  && echo "${@}"
+
+BUILD_TESTS_FLAG=FALSE
+GENERATE_API_DOC_FLAG=FALSE
+
+USER_ARG=""
+
+for arg in "$@"; do
+  case $arg in
+  -h|--help)
+    print_help_in_terminal
+    exit
+    ;;
+  --compile-test)
+    BUILD_TESTS_FLAG=TRUE
+    shift
+    ;;
+  --generate-doc)
+    GENERATE_API_DOC_FLAG=TRUE
+    shift
+    ;;
+  --) # no more option
+    shift
+    break
+    ;;
+  -?* | --?*)
+    echo "$0: $1: unrecognized option" >&2 # Note: '>&2' = print to stderr
+    ;;
+  *) # Default case
+    break
+    ;;
+  esac
+
+  shift
+done
+
+
+# ==== Install tools =============================================================================================
+sudo apt-get update &&
+  sudo apt-get install --assume-yes \
+    libyaml-cpp-dev \
+    doxygen \
+    texlive-full &&
+  sudo rm -rf /var/lib/apt/lists/*
+
+# ==== Install libpointmatcher ====================================================================================
+# https://github.com/ethz-asl/libpointmatcher/tree/master
+
+git clone https://github.com/ethz-asl/libpointmatcher.git &&
+  cd libpointmatcher &&
+  mkdir build && cd build &&
+  cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo \
+    -D BUILD_TESTS=${BUILD_TESTS_FLAG} \
+    -D GENERATE_API_DOC=${GENERATE_API_DOC_FLAG} \
+    .. &&
+  make -j $(nproc) &&
+  sudo make install
+
+#   -DCMAKE_INSTALL_PREFIX=/usr/local/ \
+#    && git checkout 1.3.1 \
+
+# ==== Execute libpointmatcher unit-test===========================================================================
+if [ ${BUILD_TESTS_FLAG} == 'TRUE' ]; then
+  cd "${INSTALLED_LIBRARIES_PATH}/libpointmatcher/build"
+  utest/utest --path "${INSTALLED_LIBRARIES_PATH}/libpointmatcher/examples/data/"
+fi
+
+## ToDo: assessment >> dont think it should be part of the libpointmatcher install script
+#cd "${INSTALLED_LIBRARIES_PATH}"
+#git clone https://github.com/norlab-ulaval/norlab_icp_mapper.git \
+#    && mkdir -p norlab_icp_mapper/build && cd norlab_icp_mapper/build \
+#    && cmake -DCMAKE_BUILD_TYPE=Release .. \
+#    && make -j $(nproc) \
+#    && sudo make install
