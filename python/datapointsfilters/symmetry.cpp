3511494445453246dd0a16c1cf38be7c0de4d1ba
boxanm <matej.boxan@gmail.com>
2023-10-10 13:06:14 -0400

    Add symmetry sampling to Python modules

---
+++ python/datapointsfilters/symmetry.cpp
@@ -0,0 +1,29 @@
+#include "symmetry.h"
+
+#include "DataPointsFilters/Symmetry.h"
+
+namespace python
+{
+	namespace datapointsfilters
+	{
+		void pybindSymmetry(py::module& p_module)
+		{
+			using SymmetryDataPointsFilter = SymmetryDataPointsFilter<ScalarType>;
+			py::class_<SymmetryDataPointsFilter, std::shared_ptr<SymmetryDataPointsFilter>, DataPointsFilter>(p_module, "SymmetryDataPointsFilter")
+
+				.def_static("description", &SymmetryDataPointsFilter::description)
+				.def_static("availableParameters", &SymmetryDataPointsFilter::availableParameters)
+
+				.def_readonly("vrs", &SymmetryDataPointsFilter::vrs)
+				.def_readonly("vro", &SymmetryDataPointsFilter::vro)
+				.def_readonly("dt", &SymmetryDataPointsFilter::dt)
+				.def_readonly("ct", &SymmetryDataPointsFilter::ct)
+				.def_readonly("knn", &SymmetryDataPointsFilter::knn)
+
+				.def(py::init<const Parameters&>(), py::arg("params") = Parameters(), "Constructor, uses parameter interface")
+
+				.def("filter", &SymmetryDataPointsFilter::filter)
+				.def("inPlaceFilter", &SymmetryDataPointsFilter::inPlaceFilter);
+		}
+	}
+}
---
+++ python/datapointsfilters/symmetry.h
@@ -0,0 +1,14 @@
+#ifndef PYTHON_DATAPOINTSFILTERS_SYMMETRY_H
+#define PYTHON_DATAPOINTSFILTERS_SYMMETRY_H
+
+#include "pypoint_matcher_helper.h"
+
+namespace python
+{
+	namespace datapointsfilters
+	{
+		void pybindSymmetry(py::module& p_module);
+	}
+}
+
+#endif //PYTHON_DATAPOINTSFILTERS_SYMMETRY_H
