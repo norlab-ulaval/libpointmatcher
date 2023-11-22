c15f4f5a7d583aae296da3c1e7d486509c1ceb0a
boxanm <matej.boxan@gmail.com>
2023-11-21 23:15:19 -0500

    Add Python bindings for AddDescriptorFilter

---
+++ python/datapointsfilters/add_descriptor.h
@@ -0,0 +1,14 @@
+#ifndef PYTHON_DATAPOINTSFILTERS_ADD_DESCRIPTOR_H
+#define PYTHON_DATAPOINTSFILTERS_ADD_DESCRIPTOR_H
+
+#include "pypoint_matcher_helper.h"
+
+namespace python
+{
+	namespace datapointsfilters
+	{
+		void pybindAddDescriptor(py::module& p_module);
+	}
+}
+
+#endif //PYTHON_DATAPOINTSFILTERS_ADD_DESCRIPTOR_H
