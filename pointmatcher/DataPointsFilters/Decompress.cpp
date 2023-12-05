43df475039782636b997b7f59bdf024813d28999
boxanm <matej.boxan@gmail.com>
2023-12-04 20:59:48 -0500

    Create class and add it to CMakeLists.txt

---
+++ pointmatcher/DataPointsFilters/Decompress.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matěj Boxan on 2023-12-04.
+//
+
+#include "Decompress.h"
---
+++ pointmatcher/DataPointsFilters/Decompress.h
@@ -0,0 +1,12 @@
+//
+// Created by Matěj Boxan on 2023-12-04.
+//
+
+#ifndef LIBPOINTMATCHER_DECOMPRESS_H
+#define LIBPOINTMATCHER_DECOMPRESS_H
+
+class Decompress
+{
+};
+
+#endif //LIBPOINTMATCHER_DECOMPRESS_H
