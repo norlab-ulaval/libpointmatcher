51f48e0d66db9aef19ea46d0d6591936604cf3d3
RedLeaderOne <luc.coupal.1@ulaval.ca>
2023-12-06 17:18:20 -0500

    refactor: move libpointmatcher build-system logic to norlab-build-system submodule [NMO-398]

--- build_system/lpm_execute_compose_over_build_matrix.bash
+++ build_system/lpm_execute_compose_over_build_matrix.bash
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-source ./nbs_execute_compose_over_build_matrix.bash "$@"
+
+# ....path resolution logic........................................................................
+NBS_PATH_TO_SRC_SCRIPT="$(realpath "${BASH_SOURCE[0]}")"
+NBS_ROOT_DIR="$(dirname "${NBS_PATH_TO_SRC_SCRIPT}")/utilities/norlab-build-system"
+
+
+#source ./nbs_execute_compose_over_build_matrix.bash "$@"
+source "${NBS_ROOT_DIR}"/src/build_scripts/nbs_execute_compose_over_build_matrix.bash "$@"
