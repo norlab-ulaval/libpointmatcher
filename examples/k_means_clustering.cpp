dfb2397261fb4b677a12f553b62832589e4e39b6
boxanm <matej.boxan@gmail.com>
2024-01-05 14:23:22 -0500

    Code formatting

--- examples/k_means_clustering.cpp
+++ examples/k_means_clustering.cpp
@@ -3,43 +3,56 @@
 #include <boost/assign.hpp>
 #include <ctime>
 #include <omp.h>
+#include <chrono>
 
 using namespace PointMatcherSupport;
 using namespace std;
-using namespace boost;
 
 typedef PointMatcher<float> PM;
 typedef PM::DataPoints DP;
 typedef PM::Parameters Parameters;
+
+template<
+        class result_t   = std::chrono::milliseconds,
+        class clock_t    = std::chrono::steady_clock,
+        class duration_t = std::chrono::milliseconds
+>
+auto since(std::chrono::time_point<clock_t, duration_t> const& start)
+{
+    return std::chrono::duration_cast<result_t>(clock_t::now() - start).count() / 1000.0;
+}
 
 int main()
 {
     DP in(DP::load("apartment.vtk"));
 
     std::shared_ptr<PM::DataPointsFilter> kmeansfilter =
-        PM::get().DataPointsFilterRegistrar.create(
-                "KMeansClusteringDataPointsFilter",
-                {
-                    {"k", toParam(4000)},
-                    {"iter", toParam(4)},
-                    {"epsilon", toParam(0.001)},
-                    {"seed", toParam(-1)}
-                }
-        );
+            PM::get().DataPointsFilterRegistrar.create(
+                    "KMeansClusteringDataPointsFilter",
+                    {
+                            {"k",       toParam(4000)},
+                            {"iter",    toParam(4)},
+                            {"epsilon", toParam(0.001)},
+                            {"seed",    toParam(-1)}
+                    }
+            );
 
     cout << "starting k-means clustering filter" << endl;
+    auto start = chrono::steady_clock::now();
     clock_t time_a = clock();
     auto out = kmeansfilter->filter(in);
     clock_t time_b = clock();
 
-    if (time_a == ((clock_t)-1) || time_b == ((clock_t)-1))
+    if(time_a == ((clock_t)-1) || time_b == ((clock_t)-1))
     {
         perror("Unable to calculate elapsed time");
         return -1;
     }
     else
     {
-        cout << "Performed k-means clustering in " << (float)(time_b - time_a)/CLOCKS_PER_SEC << " seconds" << endl;
+        unsigned number_of_threads = std::max(std::atoi(std::getenv("OMP_NUM_THREADS")), 1);
+        cout << "Performed k-means clustering in " << (float)(time_b - time_a) / CLOCKS_PER_SEC << " seconds, distributed over " << number_of_threads << " threads." << endl;
+        cout << "Elapsed time " << since(start) << " seconds." << endl;
     }
     out.save("kmeans_out.vtk");
     return 0;
--- pointmatcher/DataPointsFilters/KMeansClustering.cpp
+++ pointmatcher/DataPointsFilters/KMeansClustering.cpp
@@ -3,25 +3,25 @@
 
 #include "KMeansClustering.h"
 
-template <typename T>
+template<typename T>
 KMeansClusteringDataPointsFilter<T>::KMeansClusteringDataPointsFilter(const Parameters& params) :
-	PointMatcher<T>::DataPointsFilter("KMeansClusteringDataPointsFilter",
-		KMeansClusteringDataPointsFilter::availableParameters(), params),
-	k(Parametrizable::get<std::size_t>("k")),
-	iter(Parametrizable::get<std::size_t>("iter")),
-	epsilon(Parametrizable::get<T>("epsilon")),
-    seed(Parametrizable::get<int>("seed"))
+        PointMatcher<T>::DataPointsFilter("KMeansClusteringDataPointsFilter",
+                                          KMeansClusteringDataPointsFilter::availableParameters(), params),
+        k(Parametrizable::get<std::size_t>("k")),
+        iter(Parametrizable::get<std::size_t>("iter")),
+        epsilon(Parametrizable::get<T>("epsilon")),
+        seed(Parametrizable::get<int>("seed"))
 {
 }
 
 // KMeansClusteringDataPointsFilter
 template<typename T>
 typename PointMatcher<T>::DataPoints KMeansClusteringDataPointsFilter<T>::filter(
-	const DataPoints& input)
+        const DataPoints& input)
 {
-	DataPoints output(input);
-	inPlaceFilter(output);
-	return output;
+    DataPoints output(input);
+    inPlaceFilter(output);
+    return output;
 }
 
 // In-place filter
@@ -31,17 +31,16 @@ void KMeansClusteringDataPointsFilter<T>::inPlaceFilter(
 {
     // get k seed points for means clustering
     DataPoints seeds = getSeedPoints(cloud);
-    std::cout << seeds.getNbPoints() << std::endl;
+    std::cout << "Number of seed points: " << seeds.getNbPoints() << std::endl;
     // create new descriptor with the seed point index
     Vector cluster_index = Vector::Zero(cloud.getNbPoints());
     assignClusters(cloud, seeds, cluster_index);
 
     float clusters_epsilon = epsilon + 1.0;
     size_t ctr = 0;
-    while (clusters_epsilon > epsilon && ctr < iter)
+    while(clusters_epsilon > epsilon && ctr < iter)
     {
         ctr++;
-        std::cout << ctr << std::endl;
 
         // reset seed statistics
         seeds.features.setZero();
@@ -49,7 +48,7 @@ void KMeansClusteringDataPointsFilter<T>::inPlaceFilter(
 
         seeds.descriptors.setZero();
 
-        #pragma omp parallel for
+#pragma omp parallel for
         for(unsigned i = 0; i < cloud.getNbPoints(); ++i)
         {
             unsigned cluster = cluster_index(i);
@@ -64,11 +63,10 @@ void KMeansClusteringDataPointsFilter<T>::inPlaceFilter(
     cloud.addDescriptor("cluster_index", cluster_index.transpose());
 }
 
-
 template<typename T>
 void KMeansClusteringDataPointsFilter<T>::assignClusters(const DataPoints& cloud, const DataPoints& seeds, Vector& cluster_index)
 {
-    #pragma omp parallel for
+#pragma omp parallel for
     for(unsigned i = 0; i < cloud.getNbPoints(); ++i)
     {
         double min_distance = (cloud.features.col(i) - seeds.features.col(0)).norm();
@@ -76,7 +74,7 @@ void KMeansClusteringDataPointsFilter<T>::assignClusters(const DataPoints& cloud
         for(unsigned j = 1; j < seeds.getNbPoints(); ++j)
         {
             double dist = (cloud.features.col(i) - seeds.features.col(j)).norm();
-            if (dist < min_distance)
+            if(dist < min_distance)
             {
                 min_distance = dist;
                 min_distance_index = j;
@@ -89,16 +87,18 @@ void KMeansClusteringDataPointsFilter<T>::assignClusters(const DataPoints& cloud
 template<typename T>
 typename PointMatcher<T>::DataPoints KMeansClusteringDataPointsFilter<T>::getSeedPoints(const DataPoints& cloud)
 {
-    double prob = double (k) / cloud.getNbPoints();
+    double prob = double(k) / cloud.getNbPoints();
     std::shared_ptr<typename PointMatcher<T>::DataPointsFilter> randomSample =
-        PM::get().DataPointsFilterRegistrar.create(
-                "RandomSamplingDataPointsFilter",
-                {{"prob", std::to_string(prob)},
-                 {"seed", std::to_string(seed)}}
-        );
+            PM::get().DataPointsFilterRegistrar.create(
+                    "RandomSamplingDataPointsFilter",
+                    {{"prob", std::to_string(prob)},
+                     {"seed", std::to_string(seed)}}
+            );
 
     return randomSample->filter(cloud);
 }
 
-template struct KMeansClusteringDataPointsFilter<float>;
-template struct KMeansClusteringDataPointsFilter<double>;
+template
+struct KMeansClusteringDataPointsFilter<float>;
+template
+struct KMeansClusteringDataPointsFilter<double>;
