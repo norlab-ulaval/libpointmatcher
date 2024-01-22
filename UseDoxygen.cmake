e696a9ac2ca49d09b1fa643e315210451856f0ca
RedLeaderOne <luc.coupal.1@ulaval.ca>
2024-01-22 13:19:57 -0500

    fix: CMake error on doxygen doc compilation [github issue 551] [YouTrack NMO-478]

--- UseDoxygen.cmake
+++ UseDoxygen.cmake
@@ -109,8 +109,7 @@ if(DOXYGEN_FOUND AND DOXYFILE_IN_FOUND)
 
 	configure_file(${DOXYFILE_IN} Doxyfile IMMEDIATE @ONLY)
 
-	get_target_property(DOC_TARGET doc TYPE)
-	if(NOT DOC_TARGET)
+	if(NOT TARGET doc)
 		add_custom_target(doc)
 	endif()
 
